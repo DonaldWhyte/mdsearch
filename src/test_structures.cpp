cfe2d71af7262e8a09c4e69f97b7ba7fa81f3ea9
Donald Whyte <dwhyte@helios.(none)>
2014-05-26 18:28:01 +0100

    Added performance tests which provide execution times of insertions, deletions and point queries for each structure, alongside the correctness tests.

--- src/test_structures.cpp
+++ src/test_structures.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "dataset.hpp"
+#include "timing.hpp"
 #include "kdtree.hpp"
 #include "multigrid.hpp"
 #include "bithash.hpp"
@@ -9,7 +10,7 @@ using namespace mdsearch;
 
 // Test parameters
 static const int NUM_DIMENSIONS = 10;
-static const int NUM_TEST_POINTS = 10000;
+static const int NUM_TEST_POINTS = 100000;
 typedef std::vector< Point<NUM_DIMENSIONS> > PointList;
 
 /* Functions used to generate random test dataset. */
@@ -93,6 +94,88 @@ void testStructure(const std::string& structureName, STRUCT_TYPE* structure, con
 		std::cout << "...FAILED." << std::endl;
 }
 
+template<typename STRUCT_TYPE>
+void timeStructure(const std::string& structureName, STRUCT_TYPE* structure, const PointList& points)
+{
+	static const double MAX_EXECUTION_TIME = 1800.0; // in seconds
+	static const int opsBetweenChecks = 300; // in # of operations
+
+	std::cout << "TIMING " << structureName << "..." << std::endl;
+
+	double start = getTime();
+	for (unsigned int i = 0; (i < points.size()); i++)
+	{
+		structure->insert(points[i]);
+		if ((i % opsBetweenChecks) == 0)
+		{
+			if ((getTime() - start) > MAX_EXECUTION_TIME)
+			{
+				std::cout << "\tAborted operation at point " << (i + 1) << std::endl;
+				break;
+			}
+		}
+	}
+	std::cout << "\tInsertion took " << getTime() - start << " seconds" << std::endl;
+
+	start = getTime();
+	for (unsigned int i = 0; (i < points.size()); i++)
+	{
+		structure->query(points[i]);
+		if ((i % opsBetweenChecks) == 0)
+		{
+			if ((getTime() - start) > MAX_EXECUTION_TIME)
+			{
+				std::cout << "\tAborted operation at point " << (i + 1) << std::endl;
+				break;
+			}
+		}
+	}
+	std::cout << "\tQueries took " << getTime() - start << " seconds" << std::endl;
+	
+	start = getTime();
+	for (unsigned int i = 0; (i < points.size()); i++)
+	{
+		structure->remove(points[i]);
+		if ((i % opsBetweenChecks) == 0)
+		{
+			if ((getTime() - start) > MAX_EXECUTION_TIME)
+			{
+				std::cout << "\tAborted operation at point " << (i + 1) << std::endl;
+				break;
+			}
+		}
+	}
+	std::cout << "\tDeletion took " << getTime() - start << " seconds" << std::endl;
+
+	std::cout << "...DONE." << std::endl;
+}
+
+void testCorrectness(const PointList& points, const Boundary<NUM_DIMENSIONS>& boundary)
+{
+	std::cout << "-----------------CORRECTNESS TESTS-----------------" << std::endl;
+	KDTree<NUM_DIMENSIONS> kdTree;
+	testStructure< KDTree<NUM_DIMENSIONS> >("kd-tree", &kdTree, points);
+	Multigrid<NUM_DIMENSIONS> multigrid(boundary);
+	testStructure< Multigrid<NUM_DIMENSIONS> >("multigrid", &multigrid, points);
+	BitHash<NUM_DIMENSIONS> bitHash;
+	testStructure< BitHash<NUM_DIMENSIONS> >("bithash", &bitHash, points);
+	PyramidTree<NUM_DIMENSIONS> pyramidTree(boundary);
+	testStructure< PyramidTree<NUM_DIMENSIONS> >("pyramid_tree", &pyramidTree, points);
+}
+
+void testPerformance(const PointList& points, const Boundary<NUM_DIMENSIONS>& boundary)
+{
+	std::cout << "-----------------PERFORMANCE TESTS-----------------" << std::endl;
+	KDTree<NUM_DIMENSIONS> kdTree;
+	timeStructure< KDTree<NUM_DIMENSIONS> >("kd-tree", &kdTree, points);
+	Multigrid<NUM_DIMENSIONS> multigrid(boundary);
+	timeStructure< Multigrid<NUM_DIMENSIONS> >("multigrid", &multigrid, points);
+	BitHash<NUM_DIMENSIONS> bitHash;
+	timeStructure< BitHash<NUM_DIMENSIONS> >("bithash", &bitHash, points);
+	PyramidTree<NUM_DIMENSIONS> pyramidTree(boundary);
+	timeStructure< PyramidTree<NUM_DIMENSIONS> >("pyramid_tree", &pyramidTree, points);
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -102,14 +185,9 @@ int main(int argc, char* argv[])
 	dataset.load( generateRandomPoints(NUM_TEST_POINTS) );
 	Boundary<NUM_DIMENSIONS> boundary = dataset.computeBoundary();
 
-	KDTree<NUM_DIMENSIONS> kdTree;
-	testStructure< KDTree<NUM_DIMENSIONS> >("kd-tree", &kdTree, dataset.getPoints());
-	Multigrid<NUM_DIMENSIONS> multigrid(boundary);
-	testStructure< Multigrid<NUM_DIMENSIONS> >("multigrid", &multigrid, dataset.getPoints());
-	BitHash<NUM_DIMENSIONS> bitHash;
-	testStructure< BitHash<NUM_DIMENSIONS> >("bithash", &bitHash, dataset.getPoints());
-	PyramidTree<NUM_DIMENSIONS> pyramidTree(boundary);
-	testStructure< PyramidTree<NUM_DIMENSIONS> >("pyramid_tree", &pyramidTree, dataset.getPoints());
+	testCorrectness(dataset.getPoints(), boundary);
+	std::cout << std::endl;
+	testPerformance(dataset.getPoints(), boundary);
 
 	return 0;
 }
