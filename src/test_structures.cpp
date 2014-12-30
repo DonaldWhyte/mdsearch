29d8e5098cf89c2f2d5937dc5b6dd5a0c28a954b
Donald Whyte <donaldwhyte0@gmail.com>
2014-12-30 15:56:04 +0000

    Added comment headers to two test files to descirbe what they do.

--- src/test_structures.cpp
+++ src/test_structures.cpp
@@ -1,3 +1,44 @@
+/******************************************************************************
+
+mdsearch - Lightweight C++ library implementing a collection of
+           multi-dimensional search structures
+
+File:        test_structures.cpp
+Description: Test executable that generates a random point dataset and uses
+             it to test the correctness of all the index structures implemented
+             in mdsearch.
+
+             In addition to correctness tests, this executable also runs the
+             structures through some basic timing tests. It outputs the total
+             time it takes the structures to insert, delete or find each point
+             in the randomly generated dataset.
+
+*******************************************************************************
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Donald Whyte
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+******************************************************************************/
+
 #include "dataset.hpp"
 #include "timing.hpp"
 #include "kdtree.hpp"
